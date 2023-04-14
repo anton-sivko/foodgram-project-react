823815f895d0418d4343dd6541c0b4cd85b3c8b6
Anton <asivko@gmail.com>
2023-04-14 22:01:25 +0200

    users editing

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LIMIT_USERNAME = 150
+LIMIT_EMAIL = 254
